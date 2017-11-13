83a850337586a050ad455e3d0931a0c70a013285
Joel Ramirez <joelramirez@kubesoft.com>
2017-11-13 12:43:26 -0400

    creacion de las rutas de los modelos iniciales

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,63 @@ use Illuminate\Http\Request;
 * Users
 **/
 Route::resource('users', 'User\UserController', ['except' => ['create', 'edit']]);
+
+/**
+* Countries
+**/
+Route::resource('countries', 'Country\CountryController', ['except' => ['create', 'edit']]);
+
+/**
+* Departments
+**/
+Route::resource('departments', 'Department\DepartmentController', ['except' => ['create', 'edit']]);
+
+/**
+* Cities
+**/
+Route::resource('cities', 'City\CityController', ['except' => ['create', 'edit']]);
+
+/**
+* Licence Plans
+**/
+Route::resource('licence_plans', 'LicencePlan\LicencePlanController', ['except' => ['create', 'edit']]);
+
+/**
+* Companies
+**/
+Route::resource('companies', 'Company\CompanyController', ['except' => ['create', 'edit']]);
+
+/**
+* Pay Options
+**/
+Route::resource('pay_options', 'PayOption\PayOptionController', ['except' => ['create', 'edit']]);
+
+/**
+* Suppliers
+**/
+Route::resource('suppliers', 'Supplier\SupplierController', ['except' => ['create', 'edit']]);
+
+/**
+* Customers
+**/
+Route::resource('customers', 'Customer\CustomerController', ['except' => ['create', 'edit']]);
+
+/**
+* Product Categories
+**/
+Route::resource('product_categories', 'ProductCategory\ProductCategoryController', ['except' => ['create', 'edit']]);
+
+/**
+* Taxes
+**/
+Route::resource('taxes', 'Tax\TaxController', ['except' => ['create', 'edit']]);
+
+/**
+* Products
+**/
+Route::resource('products', 'Product\ProductController', ['except' => ['create', 'edit']]);
+
+/**
+* Warehouses
+**/
+Route::resource('warehouses', 'Warehouse\WarehouseController', ['except' => ['create', 'edit']]);
